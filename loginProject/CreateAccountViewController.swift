d271c87e6488fc61956050e6c41898eef7b876fe
Avishaan <avishaan@codeHatcher.com>
2015-05-03 16:13:35 -0700

    create a protocol in the createAccountViewController as well as a delegate (often used by never understood)

--- loginProject/CreateAccountViewController.swift
+++ loginProject/CreateAccountViewController.swift
@@ -8,14 +8,21 @@
 
 import UIKit
 
+// protocol is an extension of the create account view controller and defines functions that can be called from the CreateAccountViewController but we will implement those functions in the LoginViewController class
+protocol CreateAccountViewControllerDelegate {
+  func accountCreated()
+}
+
 class CreateAccountViewController: UIViewController {
 
   @IBOutlet weak var chooseUsernameTextField: UITextField!
   @IBOutlet weak var choosePasswordTextField: UITextField!
   @IBOutlet weak var confirmPasswordTextField: UITextField!
   
+  var delegate:CreateAccountViewControllerDelegate?
+  
   // we use this key to prevent typing errors
-  let kUserNameKey = "userNameKey"
+  let kUsernameKey = "usernameKey"
   let kPasswordKey = "passwordKey"
   
     override func viewDidLoad() {
@@ -35,12 +42,13 @@ class CreateAccountViewController: UIViewController {
       // we will store in NSUserDefaults so that we can information persistance between sessions
       // NSUserDefaults only for basic settings, in general we should not store password here
       // get access to single instance
-      NSUserDefaults.standardUserDefaults().setObject(self.chooseUsernameTextField.text, forKey: kUserNameKey)
+      NSUserDefaults.standardUserDefaults().setObject(self.chooseUsernameTextField.text, forKey: kUsernameKey)
       NSUserDefaults.standardUserDefaults().setObject(self.choosePasswordTextField.text, forKey: kPasswordKey)
       // save these defaults
       NSUserDefaults.standardUserDefaults().synchronize()
       
       self.dismissViewControllerAnimated(true, completion: nil)
+      delegate?.accountCreated()
     }
   }
   
