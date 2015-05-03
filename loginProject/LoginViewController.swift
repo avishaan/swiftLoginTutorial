8c94b94304168003b04c45381ab41e441240b286
Avishaan <avishaan@codeHatcher.com>
2015-05-03 16:36:07 -0700

    implement the CreateAccountViewDelegate protocol in the LoginViewController

--- loginProject/LoginViewController.swift
+++ loginProject/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, CreateAccountViewControllerDelegate {
 
   @IBOutlet weak var passwordTextField: UITextField!
   @IBOutlet weak var usernameTextField: UITextField!
@@ -22,7 +22,15 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  
+  // we need to set delegate property in the CreateAccountViewController Delgate
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "loginToCreateAccountSegue" {
+      var createAccountVC = segue.destinationViewController as CreateAccountViewController
+      createAccountVC.delegate = self
+    }
     
+  }
 
   @IBAction func loginButtonPressed(sender: UIButton) {
     self.performSegueWithIdentifier("loginToMainSegue", sender: self)
@@ -31,14 +39,12 @@ class LoginViewController: UIViewController {
   @IBAction func createAccountButtonPressed(sender: UIButton) {
     self.performSegueWithIdentifier("loginToCreateAccountSegue", sender: self)
   }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
+  // CreateAccountViewControllerDelegate
+  // when account is created, skip this screen and skip to main screen there are other ways to do it but this is in order to use the protocol
+  func accountCreated() {
+    self.performSegueWithIdentifier("loginToMainSegue", sender: nil)
+  }
+  
 
 }
