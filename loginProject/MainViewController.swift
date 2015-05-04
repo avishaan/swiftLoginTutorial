57ba16f1612e881585c35f509c2740a0794d6ec7
Avishaan <avishaan@codeHatcher.com>
2015-05-03 17:18:22 -0700

    have mainViewController show the login details from the NSUserDefaults

--- loginProject/MainViewController.swift
+++ loginProject/MainViewController.swift
@@ -12,27 +12,20 @@ class MainViewController: UIViewController {
   
   @IBOutlet weak var usernameLabel: UILabel!
   @IBOutlet weak var passwordLabel: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+    
+    usernameLabel.text = NSUserDefaults.standardUserDefaults().objectForKey(kUsernameKey) as? String
+    passwordLabel.text = NSUserDefaults.standardUserDefaults().objectForKey(kPasswordKey) as? String
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
 }
